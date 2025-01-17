cf31549a4470746e77f48390f3edba642d63bb89
lexborelli <alexasborelli@gmail.com>
2025-01-17 11:45:32 -0500

    retrieved the list of courses from rest api endpoint, render a list of each course that links the respective "course detail", and rendered a link to "courses create"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import Header from './components/Header';
 import Courses from './components/Courses';
 
 function App() {
+  
 
   return (
     <Router>
--- client/src/components/Courses.jsx
+++ client/src/components/Courses.jsx
@@ -1,21 +1,45 @@
+import axios from "axios"; 
+import { useEffect, useState } from "react"; 
+import { Link } from "react-router-dom";
+
 
 const Courses = () => {
+    
+    const [courses, setCourses] = useState([]);
+
+    //used useEffect() to fetch data with the help of axios which was imported to fetch courses data from api folder, gave axios access to courses endpoint to showcase all available courses
+    useEffect(() => {
+        const fetchCourses = async () => {
+            try {
+                const response = await axios.get('http://localhost:5000/api/courses');
+            //handle success
+            setCourses(response.data); 
+        }
+        catch(error) {
+            //handle error
+            console.log("Error fetching and parsing data for Courses", error);
+        }
+        };
+        fetchCourses();
+    }, []);
+    
+        // used map() to iterate over the courses array and return each courses id and title. 
+        // rendered a link to the "Create Course" screen at the route "/courses/create"
+
     return (
         <main>
             <div className="wrap main--grid">
-                <a className="course--module course--link" href="course-detail.html">
+
+                { courses.map(course => (
+                    
+                    <Link key={course.id} className="course--module course--link" to={`courses/${course.id}`}>
                     <h2 className="course--label">Course</h2>
-                    <h3 className="course--title">Build a Basic Bookcase</h3>
-                </a>
-                <a className="course--module course--link" href="course-detail.html">
-                    <h2 className="course--label">Course</h2>
-                    <h3 className="course--title">Learn How to Program</h3>
-                </a>
-                <a className="course--module course--link" href="course-detail.html">
-                    <h2 className="course--label">Course</h2>
-                    <h3 className="course--title">Learn How to Test Programs</h3>
-                </a>
-                <a className="course--module course--add--module" href="create-course.html">
+                    <h3 className="course--title">{course.title}</h3>
+                    </Link>
+
+                ))}
+
+                <Link className="course--module course--add--module" href="create-course.html">
                     <span className="course--add--title">
                         <svg
                             version="1.1"
@@ -29,10 +53,11 @@ const Courses = () => {
                         </svg>
                         New Course
                     </span>
-                </a>
+                </Link>
             </div>
         </main>
     )
-};  
+};
+
 
 export default Courses; 
