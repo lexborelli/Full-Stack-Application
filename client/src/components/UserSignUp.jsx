543510f86a9f9a322d2f352c94e9da5b081e8f6e
lexborelli <alexasborelli@gmail.com>
2025-01-29 16:22:48 -0500

    created handleSubmit and handle Cancel event handlers. In handleSubmit, created user variable to store users infor that they input on signup form. created a try catch block to response to errors and if server creates user.

--- client/src/components/UserSignUp.jsx
+++ client/src/components/UserSignUp.jsx
@@ -1,10 +1,68 @@
-import { Link } from "react-router-dom";
+import { useContext, useRef, useState } from "react"; 
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios"; 
 
 const UserSignUp = () => {
+    //hooks
+    const navigate = useNavigate();
+
+    //state
+    const firstName = useRef(null);
+    const lastName = useRef(null);
+    const emailAddress = useRef(null);
+    const password = useRef(null); 
+    const [error, setErrors] = useState([]); 
+
+    //eventhandlers
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        //created a user variable that contains the user's first and last name, email address, and password.
+
+        const user = {
+            firstName: firstName.current.value, 
+            lastName: lastName.current.value, 
+            emailAddress: emailAddress.current.value, 
+            password: password.current.value
+        }
+
+        //created a try catch block to handle errors
+
+        try {
+            // make a post request to the server to create a new user
+            const response = await axios.post("http://localhost:5000/api/users", user);
+    
+            //if servers response status of 201 then user was created, log user friendly message to console 
+            // if server return response status of 400 then I stored the data in the Api will respond with the error 
+            //to catch any other response that doesn't have a status code of 201 or 400
+
+            if (response.status === 201) {
+            console.log(`${user.firstName} is successfully signed up and authenticated!`);
+            } else if (response.status === 400) {
+            setErrors(error.response.data);
+            } else {
+                throw new Error(); 
+            }
+        } catch (error) {
+            console.log(error);
+            navigate("/");
+        }
+    };
+
+
+    //navigate back to homepage when cancel button is clicked
+    const handleCancel = (event) => {
+        event.preventDefault();
+        navigate("/");
+    };
+
+
+
     return (
         <div className="form--centered">
                 <h2>Sign Up</h2>
-                <form>
+                <form onSubmit={handleSubmit}>
                 <label htmlFor="firstName">
                     First Name
                 </label>
@@ -12,7 +70,8 @@ const UserSignUp = () => {
                     id="firstName" 
                     name="firstName" 
                     type="text" 
-                    defaultValue="" 
+                    ref={firstName}
+                    placeholder="First Name" 
                 />
                 <label htmlFor="lastName">
                     Last Name
@@ -21,7 +80,8 @@ const UserSignUp = () => {
                     id="lastName" 
                     name="lastName" 
                     type="text" 
-                    defaultValue="" 
+                    ref={lastName}
+                    placeholder="Last Name" 
                 />
                 <label htmlFor="emailAddress">
                     Email Address
@@ -29,8 +89,9 @@ const UserSignUp = () => {
                 <input 
                     id="emailAddress" 
                     name="emailAddress" 
-                    type="email" 
-                    defaultValue="" 
+                    type="email"
+                    ref={emailAddress} 
+                    placeholder="Email Address" 
                 />
                 <label htmlFor="password">
                     Password
@@ -39,14 +100,15 @@ const UserSignUp = () => {
                     id="password" 
                     name="password" 
                     type="password" 
-                    defaultValue="" 
+                    ref={password}
+                    placeholder="password" 
                 />
                 <button className="button" type="submit">
                     Sign Up
                 </button>
                 <button
                 className="button button-secondary"
-                onClick="event.preventDefault(); location.href='index.html';"
+                onClick={handleCancel}
                 >
                     Cancel
                 </button>
