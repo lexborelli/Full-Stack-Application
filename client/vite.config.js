523f10016150e56e5b5918677416371791edde0a
lexborelli <alexasborelli@gmail.com>
2025-02-21 14:08:48 -0500

    added port

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Allow access from external IPs
+    port: 4173, // Default port, or you can specify another port if needed
+  },
 })
